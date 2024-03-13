a59e8614f91256ac928ea23f17d7347668fa8116
rana-jay <jrana@cipio.ai>
2024-03-13 11:37:48 +0530

    v2

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,24 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+function formateName(value:string){
+  return "hi " + value ; 
+}
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
-export class UserProfileComponent {
-  name = "Rana jay";
-  salary = 1000000 ;
-  isButtonDisabled = false;
-  inputValue = "test";
-  inputValue2 = "test2";
 
-  onChange(e:Event){
-    const value = (e.target as HTMLInputElement).value ;
-    this.inputValue = value;
-    console.log(value);
-  }
+export class UserProfileComponent {
+
+  @Input({alias:'userName',transform:formateName}) name  = " ";
+
+  // name = "Rana jay";
+  // salary = 1000000 ;
+  // isButtonDisabled = false;
+  // inputValue = "test";
+  // inputValue2 = "test2";
+
+  // users = [
+  //   {
+  //     name:"jay",
+  //     salary:100000
+  //   },
+  //   {
+  //     name:"manthan",
+  //     salary:200000
+  //   },
+  //   {
+  //     name:"animesh",
+  //     salary:300000
+  //   }
+  // ]
+
+  // onChange(e:Event){
+  //   const value = (e.target as HTMLInputElement).value ;
+  //   this.inputValue = value;
+  //   console.log(value);
+  // }
 
 }
